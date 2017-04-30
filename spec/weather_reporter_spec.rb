f70c8c2a7b5aab8f56c999a467687c2f052b9acc
rohin-arka <surya_siwakoti@outlook.com>
2017-04-30 08:14:53 +0545

    Added test for checking version of weather_reporter write test for checking version of weather reporter to be 0.1.0

--- spec/weather_reporter_spec.rb
+++ spec/weather_reporter_spec.rb
@@ -1,7 +1,7 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe WeatherReporter do
-  it "has a version number" do
-    expect(WeatherReporter::VERSION).not_to be nil
+  it 'has a version number' do
+    expect(WeatherReporter::VERSION).not_to be('0.1.0')
   end
 end
