e5cf62d08fef7d454df1218e01deab57c1f47c9f
rohin-arka <surya_siwakoti@outlook.com>
2017-05-01 18:28:37 +0545

    Create a folder to store helper methods for rspec Created a module weather symbol added method cloudy, sunny, partial_rainy, partial_cloudy

---
+++ spec/helpers/weather_symbol.rb
@@ -0,0 +1,34 @@
+module Helpers
+  module WeatherSymbol
+    def sunny
+        '
+          \  /
+        -- ⚪  --
+          /  \
+        '
+    end
+
+    def cloudy
+        "\t .-.
+        (   ).
+        (___(__)
+        ‘ ‘ ‘ ‘
+       ‘ ‘ ‘ ‘"
+    end
+
+    def partial_rainy
+        "\t  _`/"".-.
+          ,\_(   ).
+           /(___(__)
+           ‘ ‘ ‘ ‘
+          ‘ ‘ ‘ ‘ "
+    end
+
+    def partial_cloudy
+         '          \  /
+        _ /"".-.
+          \_(   ).
+          /(___(__)'
+    end
+  end
+end
