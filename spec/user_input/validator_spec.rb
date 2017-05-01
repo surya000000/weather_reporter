4c4d43a6ce569b091d3c978465e4fea09c5b97a5
rohin-arka <surya_siwakoti@outlook.com>
2017-05-01 16:12:59 +0545

    write test for validator class write test for validate_input method write test for get_data metho write test for input_arguments metho

--- spec/user_input/validator_spec.rb
+++ spec/user_input/validator_spec.rb
@@ -3,19 +3,26 @@ module WeatherReporter
     RSpec.describe Validator do
       let(:validator_class) { WeatherReporter::UserInput::Validator }
 
-      describe '#pair_flag_argument' do
+      describe '#input_arguments' do
         it 'gets pair of array with flag and argument' do
-          flag_argument = validator_class.new(%w{-city kathmandu -day 10}).pair_flag_argument
+          flag_argument = validator_class.new(%w{-city kathmandu -day 10}).input_arguments
           expect(flag_argument).to(eq([['-city', 'kathmandu'], ['-day', '10']]))
         end
       end
 
       describe '#validate_input' do
         it 'throws exception for invalid input' do
-          validate_input = validator_class.new(%w{-c kathmandu -d 10})
+          input = validator_class.new(%w{-c kathmandu -d 10})
           expect do
-            raise validate_input.validate_input
+            raise input.validate
           end.to(raise_error.with_message('Please enter a valid flag'))
+        end
+      end
+
+      describe '#get_data' do
+        it 'returns valid data' do
+          expect(validator_class.new(%w{-city kathmandu -day 10}).input_arguments)
+                .to(eq([['-city', 'kathmandu'], ['-day', '10']]))
         end
       end
     end
