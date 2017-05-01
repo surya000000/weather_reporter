26e74f8d9a3784df883c59029d2ca3a783550b9d
Dorje <ku.dorjee@gmail.com>
2017-05-01 11:50:02 +0545

    Test to send request to the API test the objects object's data and configuration test case for report method

--- spec/http_service/request_spec.rb
+++ spec/http_service/request_spec.rb
@@ -1,12 +1,28 @@
 require 'spec_helper'
-
 module WeatherReporter
   module HTTPService
     RSpec.describe Request do
-      it 'send request to the api' do
-        get 'http://api.apixu.com/v1/current.json?key=5f7c4e53a0d645d382a14830173004&q=Kathmandu'
-        expect(response).to be_success
+      require 'pry'
+      let(:data) {{city: "kathmandu"}}
+      let(:config) {WeatherReporter::Configuration.new.read_file }
+      let(:request) do
+        WeatherReporter::HTTPService::Request.new({city: "kathmandu"}, config)
       end
+
+
+
+      describe '.initialize' do
+        it 'set data of the object from user' do
+          expect(request.data).to eq(data)
+        end
+      end
+
+      describe '#report' do
+        it 'send data to the api' do
+          expect(request.report).to eq(0)
+        end
+      end
+
     end
   end
 end
