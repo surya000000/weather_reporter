0e20f549aa0299c1a7f6aa4c8b627c18ca106b5b
Dorje <ku.dorjee@gmail.com>
2017-05-01 10:18:35 +0545

    test case for Request class test request to hit the api

--- spec/http_service/request_spec.rb
+++ spec/http_service/request_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 module WeatherReporter
   module HTTPService
     RSpec.describe Request do
+      it 'send request to the api' do
+        get 'http://api.apixu.com/v1/current.json?key=5f7c4e53a0d645d382a14830173004&q=Kathmandu'
+        expect(response).to be_success
+      end
     end
   end
 end
