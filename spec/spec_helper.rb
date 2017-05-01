cee991e8073f0282c4f52d5cc5a1ac7d35b8dffd
Dorje <ku.dorjee@gmail.com>
2017-05-01 19:08:10 +0545

    mock the response body

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,10 @@ require "webmock/rspec"
 WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
+  # json = JSON.parse(File.read("spec/fixtures/weather.json"))
   config.before(:each) do
     stub_request(:get, "http://api.apixu.com/v1/current.json?key=5f7c4e53a0d645d382a14830173004&q=kathmandu").
     with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-    to_return(status: 200, body: "", headers: {})
+    to_return(status: 200, body: '{"location": {"name": "Kathmandu"} }', headers: {})
   end
 end
