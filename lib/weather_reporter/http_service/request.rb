512fe54ab7af80ff609e0be62efe4677870ece14
Dorje <ku.dorjee@gmail.com>
2017-05-01 11:46:52 +0545

    manage code remove binding.pry add space between methods

--- lib/weather_reporter/http_service/request.rb
+++ lib/weather_reporter/http_service/request.rb
@@ -8,9 +8,9 @@ module WeatherReporter
         @configuration = config_object.read_file
         @data = data
       end
+
       def report
         responce = HTTParty.get("http://api.apixu.com/v1/current.json?key=5f7c4e53a0d645d382a14830173004&q=Kathmandu")
-        binding.pry
       end
     end
   end
