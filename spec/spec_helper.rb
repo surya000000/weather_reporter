f43c0fe0464f38362efa116dc8c450c4cf5529f8
Dorje <ku.dorjee@gmail.com>
2017-05-01 17:18:35 +0545

    Add Webmock gem configurarion stub request to api disable extranel request

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "weather_reporter"
+require "webmock/rspec"
+WebMock.disable_net_connect!(allow_localhost: true)
+
+RSpec.configure do |config|
+  config.before(:each) do
+    stub_request(:get, "http://api.apixu.com/v1/current.json?key=5f7c4e53a0d645d382a14830173004&q=kathmandu").
+    with(headers: {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+    to_return(status: 200, body: "", headers: {})
+  end
+end
