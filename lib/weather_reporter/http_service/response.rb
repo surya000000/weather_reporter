13ee10704826e1b4837f7e63e4d489fd84be4df2
rohin-arka <surya_siwakoti@outlook.com>
2017-05-02 10:42:16 +0545

    Added response class as test

--- lib/weather_reporter/http_service/response.rb
+++ lib/weather_reporter/http_service/response.rb
@@ -1,7 +1,17 @@
 require 'ostruct'
+require 'json'
 module WeatherReporter
   module HTTPService
-    class Response < OpenStruct
+    class Response
+      attr_reader :data
+
+      def initialize(data)
+        @data = data
+      end
+
+      def data
+        JSON.parse(@data.to_json, object_class: OpenStruct)
+      end
     end
   end
 end
