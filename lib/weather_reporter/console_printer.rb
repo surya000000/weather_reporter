c8cdbff9ab69e0385b75ef259df564ca32732a2d
Dorje <ku.dorjee@gmail.com>
2017-05-02 13:37:03 +0545

    Print current weather condition gets weather codtion from weather_obj form the map weather condition to the relevent terminal icon

--- lib/weather_reporter/console_printer.rb
+++ lib/weather_reporter/console_printer.rb
@@ -1,11 +1,30 @@
 module WeatherReporter
+  include OutputFormatter
+
   class ConsolePrinter
     def initialize(weather_obj)
       @weather_obj = weather_obj
     end
 
     def print_to_console
-      @weather_reporter
+       puts(condition_map[:"#{weather_condition}"])
     end
+
+    def weather_condition
+      @weather_obj.current.condition.text
+    end
+
+    def condition_map
+      {
+        "Sunny": sunny,
+        "Overcast": cloudy,
+        "Mist": mist,
+        "Partly cloudy": partly_cloudy,
+        "Light rain": light_rain,
+        "Light rain shower": light_rain,
+        'Patchy rain possible': light_rain
+      }
+    end
+
   end
 end
