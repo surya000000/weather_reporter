a8a2b7a0d005c31dc232381d3a4e05a189b70553
rohin-arka <surya_siwakoti@outlook.com>
2017-05-02 12:23:47 +0545

    Created a class console printer Added a instance method print_to_console

---
+++ lib/weather_reporter/console_printer.rb
@@ -0,0 +1,11 @@
+module WeatherReporter
+  class ConsolePrinter
+    def initialize(weather_obj)
+      @weather_obj = weather_obj
+    end
+
+    def print_to_console
+      @weather_reporter
+    end
+  end
+end
