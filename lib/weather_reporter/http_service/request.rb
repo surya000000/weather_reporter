2ec8a600f7c6298941bda3511af3c081fe1074cf
rohin-arka <surya_siwakoti@outlook.com>
2017-05-02 09:04:50 +0545

    Refactor request class as current test cases

--- lib/weather_reporter/http_service/request.rb
+++ lib/weather_reporter/http_service/request.rb
@@ -4,29 +4,41 @@ module WeatherReporter
   module HTTPService
     class Request
       attr_reader :data, :configuration
+
       def initialize(data, config_object)
-        @configuration = config_object.read_file
+        @configuration = config_object.read_file["APIXU"]
         @data = data
       end
 
       def report
-        responce = HTTParty.get(url)
+        response = HTTParty.get(api_url)
       end
 
       private
 
-      def url
-        base_url = @configuration["APIXU"]["BASE_URL"]
-        key = @configuration["APIXU"]["API_KEY"]
-        request_type = @configuration["APIXU"]["REQUEST_TYPE"]
-        data = @data
-        if data[:day]
-          base_url + request_type["forcast"] + '?key='+ key + '&q=' + data[:city] + 'day=' + data[:day]
-        else
-          base_url + request_type["current"] + '?key='+ key + '&q=' + data[:city]
-        end
+      def api_url
+        @data[:day] ? generate_forecast_url : generate_currentl_url
       end
 
+      def generate_forecast_url
+        "#{base_url}#{request_type["forcast"]}?key=#{key}&q=#{data[:city]}day=#{data[:day]}"
+      end
+
+      def generate_currentl_url
+        "#{base_url}#{request_type["current"]}?key=#{key}&q=#{data[:city]}"
+      end
+
+      def request_type
+        @configuration["REQUEST_TYPE"]
+      end
+
+      def base_url
+        @configuration["BASE_URL"]
+      end
+
+      def key
+        @configuration["API_KEY"]
+      end
     end
   end
 end
