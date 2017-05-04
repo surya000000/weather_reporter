231a277ecedac3214e8f980305d528a68872eae8
rohin-arka <surya_siwakoti@outlook.com>
2017-05-04 15:21:48 +0545

    Raise exception if argument is empty

--- lib/weather_reporter/user_input/validator.rb
+++ lib/weather_reporter/user_input/validator.rb
@@ -2,9 +2,12 @@ module WeatherReporter
   module UserInput
     class Validator
 
-      VALID_FLAGS = %w{-city -forecast -help}
+      VALID_FLAGS = %w{-city -forecast -help}.freeze
 
-      ERROR_MESSAGE = %Q{Please enter a valid flag or Enter -help for more details}
+      ERROR_MESSAGE = %q{Please enter a valid flag or Enter -help for more details}.freeze
+
+      MESSAGE = %Q{ Please enter a Arguments eg: weather -city kathmandu \
+                 weather -city kathmandu -forecast }.freeze
 
       private_constant :VALID_FLAGS, :ERROR_MESSAGE
 
@@ -15,6 +18,7 @@ module WeatherReporter
       end
 
       def validate
+        raise WeatherReporter::Error::InvalidFlag, MESSAGE if @input_arguments.empty?
         input_arguments.each do |flag_argument|
           unless @input_arguments.include?('-city') && VALID_FLAGS.include?(flag_argument[0])
             raise WeatherReporter::Error::InvalidFlag, ERROR_MESSAGE
