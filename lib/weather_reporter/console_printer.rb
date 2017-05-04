9352c8a822f37f32b6a138e8858a9eaa6c4c87b1
rohin-arka <surya_siwakoti@outlook.com>
2017-05-04 15:17:28 +0545

    Refactor long method Refactor - printing in code which was used multiple times extract into method print_dash_30 Change condition mapper to WEATHER_TEXT constant and made immutable

--- lib/weather_reporter/console_printer.rb
+++ lib/weather_reporter/console_printer.rb
@@ -3,22 +3,30 @@ module WeatherReporter
 
   class ConsolePrinter
 
-    ERROR_MESSAGE = 'Sorry weather report cannot generated'
+    ERROR_MESSAGE = 'Sorry weather report cannot generated'.freeze
+
+    WEATHER_TEXT =  {
+                     'Sunny': :sunny, 'Cloudy': :cloudy, 'Overcast': :cloudy, 'Mist': :mist,
+                     'Partly cloudy': :partly_cloudy, 'Light rain': :light_rain,
+                     'Light rain shower': :light_rain,
+                     'Patchy rain possible': :light_rain, 'Light snow': :light_snow, 'Clear': :clear,
+                     'Moderate or heavy snow showers': :snow, 'Fog': :fog,
+                     'Thundery outbreaks possible': :light_thunder, 'Thundery': :thunder_storm
+                    }.freeze
 
     def initialize(weather_obj)
       @weather_obj = weather_obj
     end
 
     def print_to_console
-      return error if error
+      return puts error if error
       puts current_weather
-      puts forecast_summary if @weather_obj.forecast
+      forecast_summary if @weather_obj.forecast
       forecast_weather if @weather_obj.forecast
     end
 
     def current_weather
-      symbols = condition_map[:"#{weather_condition}"]
-      "\e[1m\n#{symbols}\n
+      "\e[1m\n#{current_symbols}\n
          #{@weather_obj.location.name}  |  #{@weather_obj.location.country}\n
          #{@weather_obj.location.localtime}\n
          Humidity: #{@weather_obj.current.humidity}% | #{weather_condition}\n
@@ -28,8 +36,7 @@ module WeatherReporter
     end
 
     def print_report(report)
-      symbols = condition_map[:"#{report.condition.text}"]
-      "\e[1m\n#{symbols}\n
+      "\e[1m\n#{current_symbols}\n
       Time: #{report.time}\n
       Humidity: #{report.humidity}% | #{report.condition.text}\n
       Temperature: #{report.temp_c} Celcius | #{report.temp_f } Fahrenheit
@@ -39,30 +46,46 @@ module WeatherReporter
 
     def forecast_weather
       hourly_reports = forecast_data.first.hour
-      hourly_reports.values_at(7,15,23).each do |report|
-        puts print_report(report)
-      end
+      hourly_reports.values_at(7,15,23).each{ |report| puts print_report(report) }
     end
 
     def forecast_summary
-      summary = forecast_data.first.day #max,min temp of day
-      symbols = condition_map[ :"#{summary.condition.text}"]
-      "#{'-'*30}| SUMMARY |#{'-'*30}
-      \e[1m\n#{symbols}\n
+      "#{print_dash_30}| SUMMARY |#{print_dash_30}
+      \e[1m\n#{forecast_symbols}\n
       Temp in Celcius: #{summary.maxtemp_c} - #{summary.mintemp_c}(max - min) | #{summary.avgtemp_c} (avg)\n
       Avg. Humidity: #{summary.avghumidity}% | #{summary.condition.text}\n
       Sunrise: #{forecast_astro.sunrise} | Sunset: #{forecast_astro.sunset}\n
       Moonrise: #{forecast_astro.moonrise} | Moonset: #{forecast_astro.moonset}
       \e[0m
-#{'-'*30}| FORECAST |#{'-'*30} "
+      #{print_dash_30}| FORECAST |#{print_dash_30} "
+    end
+
+    def current_symbols
+      self.send(WEATHER_TEXT[weather_condition.to_sym])
+    end
+
+    def report_sumbols
+      self.send(WEATHER_TEXT[report.condition.text.to_sym])
+    end
+
+    def forecast_symbols
+      self.send(WEATHER_TEXT[summary.condition.text.to_sym])
+    end
+
+    def summary
+      forecast_data.first.day
     end
 
     def forecast_astro
       forecast_data.first.astro
     end
 
+    def print_dash_30
+      '-' * 30
+    end
+
     def forecast_data
-       @weather_obj.forecast.forecastday
+      @weather_obj.forecast.forecastday
     end
 
     def weather_condition
@@ -84,25 +107,6 @@ module WeatherReporter
     def error_message
       error_message = @weather_obj.error.message.gsub('q', 'city')
       error_message ? error_message : @weather_obj.error.message
-    end
-
-    def condition_map
-      {
-        "Sunny": sunny,
-        "Cloudy": cloudy,
-        "Overcast": cloudy,
-        "Mist": mist,
-        "Partly cloudy": partly_cloudy,
-        "Light rain": light_rain,
-        "Light rain shower": light_rain,
-        'Patchy rain possible': light_rain,
-        'Light snow': light_snow,
-        'Clear': clear,
-        'Moderate or heavy snow showers': snow,
-        'Fog': fog,
-        'Thundery outbreaks possible': light_thunder,
-        'Thundery': thunder_storm
-      }
     end
   end
 end
